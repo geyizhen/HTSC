76371dbdf2745cbc9920bb5f20b1981a0a73887d
geyizhen <381339594@qq.com>
2020-08-26 21:21:03 +0800

    add test

--- src/lesson/machine/E_Machine.java
+++ src/lesson/machine/E_Machine.java
@@ -1,9 +1,8 @@
 package lesson.machine;
 
-import java.util.HashMap;
-import java.util.Iterator;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public class E_Machine {
 
@@ -16,10 +15,18 @@ public class E_Machine {
             System.out.println(key + "    " + machine.getSpec().getProperties().get(key));
         }
     }
+    private static void print(HashMap hashMap) {
+        for (Object key : hashMap.keySet()) {
+            System.out.println(key + "    " + hashMap.get(key));
+        }
+    }
+
 
     public static void main(String[] args) {
 
+        //初始化仓库
         Inventory inventory = new Inventory();
+        //添加商品信息
         HashMap<String, String> hashMap1 = new HashMap<String, String>(){{
             put("品牌", "联想");
             put("分类","游戏本");
@@ -51,21 +58,36 @@ public class E_Machine {
             put("处理器","AMD");
         }};
 
+        //加入库存
         inventory.addMachine("00001", 2000, new MachineSpec(hashMap1));
         inventory.addMachine("00002", 3500, new MachineSpec(hashMap2));
         inventory.addMachine("00003", 4500, new MachineSpec(hashMap3));
+        inventory.addMachine("00004", 4500, new MachineSpec(hashMap4));
 
 
 
         //通过序列号查询,并打印该machine信息
-        Machine search_machine = inventory.get("00002");
-        print(search_machine);
+        System.out.println("通过序列号查询,并打印该machine信息");
+        System.out.println();
+        Machine search_machine = inventory.get("00003");
+        if(search_machine == null){
+            System.out.println("抱歉没有找到相关产品");
+        }else {
+            print(search_machine);
+        }
+        System.out.println();
 
         //通过MachineSpec查询，输出列表
+        System.out.println("通过MachineSpec查询，输出列表");
+
+        System.out.println("需要查询的商品信息");
+        print(hashMap4);
         List list =  inventory.search(new MachineSpec(hashMap4));
-
+        if(list.size() == 0){
+            System.out.println("抱歉没有找到相关产品");
+        }else {
+            System.out.println("共查到" + list.size() + "个商品");
+        }
     }
-
-
 
 }
